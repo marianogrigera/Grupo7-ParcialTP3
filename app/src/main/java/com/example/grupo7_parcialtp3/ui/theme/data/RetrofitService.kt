1f708ee518b01e33447f3b09d563833c86a31cf5
owendonn1 <owendonn@gmail.com>
2024-10-11 14:34:10 -0300

    retrofit

--- app/src/main/java/com/example/grupo7_parcialtp3/ui/theme/data/RetrofitService.kt
+++ app/src/main/java/com/example/grupo7_parcialtp3/ui/theme/data/RetrofitService.kt
@@ -1,6 +1,17 @@
 package com.example.grupo7_parcialtp3.ui.theme.data
 
+import retrofit2.Retrofit
+import retrofit2.create
+
 interface RetrofitService {
+
+object RetrofitServiceFactory{
+    fun makeRetrofitService(): RetrofitService{
+        return  Retrofit.Builder()
+            .baseUrl("https://fakestoreapi.com/products/1")
+            .build().create(RetrofitService::class.java)
+    }
+}
 
 
 }
