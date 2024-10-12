e6642199d7325f8d04c1097fd24dc7846a365d11
Matileoro <maturon1@hotmail.com>
2024-10-12 10:23:06 -0300

    unir

--- app/src/main/java/com/example/grupo7_parcialtp3/MainActivity.kt
+++ app/src/main/java/com/example/grupo7_parcialtp3/MainActivity.kt
@@ -1,6 +1,6 @@
-import android.os.Bundle
 
-mport android.os.Bundle
+
+import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
