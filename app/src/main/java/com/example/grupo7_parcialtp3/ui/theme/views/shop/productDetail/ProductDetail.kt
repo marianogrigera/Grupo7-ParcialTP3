574bbd197025ca5b0c5f697ac22ccd7a259cebff
SebastianGarcia21 <sebaslp_87@hotmail.com>
2024-10-15 18:21:31 -0300

    Creacion pantalla Product Detail

--- app/src/main/java/com/example/grupo7_parcialtp3/ui/theme/views/shop/productDetail/ProductDetail.kt
+++ app/src/main/java/com/example/grupo7_parcialtp3/ui/theme/views/shop/productDetail/ProductDetail.kt
@@ -17,7 +17,6 @@ import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.*
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
